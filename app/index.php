558a43fc995adc5d0278b7914ebb181506087681
adamusek-007 <adam.kuczabinski@gmail.com>
2023-11-11 20:57:08 +0100

    Create GET requests processor

--- app/index.php
+++ app/index.php
@@ -1,2 +1,15 @@
 <?php
-$_GET[]
+include("db-config.php");
+include("connection.php");
+
+$temperature = $_GET["temperature"];
+$humidity = $_GET["humidity"];
+
+if ($temperature != null && $humidity != null) {
+    $tempSql = "INSERT INTO `data` (`value`, `sensor_id`) VALUES ({$temperature}, 1);";
+    $humiditySql = "INSERT INTO `data` (`value`, `sensor_id`) VALUES ({$humidity}, 2);";
+
+    $connection = getDatabaseConnection();
+    $connection->query($tempSql);
+    $connection->query($humiditySql);
+}
