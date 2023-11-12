67426690633736a3dd09e2ae44442d3e1127b631
adamusek-007 <adam.kuczabinski@gmail.com>
2023-11-12 11:15:11 +0100

    Update arduino code to send JSON data

--- dht-11-data-reader.ino
+++ dht-11-data-reader.ino
@@ -1,4 +1,5 @@
 #include <DHT.h>
+#include "string.h"
 
 #define DHTPIN 2      // Pin where the DHT11 is connected
 #define DHTTYPE DHT11 // DHT sensor type
@@ -17,12 +18,13 @@ void loop() {
   float temperature = dht.readTemperature();
 
   if (isnan(humidity) || isnan(temperature)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
+    Serial.println(F("{\"error\": \"Failed to read from DHT sensor!\""));
     return;
   }
 
-  Serial.print(F("Temperature: "));
+  Serial.print(F("{\"temperature\": "));
   Serial.print(temperature);
-  Serial.print(F(", Humidity: "));
-  Serial.println(humidity);
+  Serial.print(F(", \"humidity\": "));
+  Serial.print(humidity);
+  Serial.println(F("}"));
 }
