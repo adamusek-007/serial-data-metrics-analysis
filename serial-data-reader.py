f029f4db68cc07bbfcbea59815e2760a45202f18
adamusek-007 <adam.kuczabinski@gmail.com>
2023-11-12 11:20:19 +0100

    Update serial-data-reader

--- serial-data-reader.py
+++ serial-data-reader.py
@@ -1,5 +1,11 @@
 import serial
 import requests
+import json
+
+COM_PORT = "COM6"
+COM_BAUDRATE = 9600
+
+# TODO reusable functions, upgrade visual aspect of the code
 
 ser = serial.Serial("COM6", 9600)
 
@@ -11,19 +17,34 @@ def send_sensor_data(temperature, humidity):
         print(f"Failed to send data. Status code: {response.status_code}")
 
 
+def process_error(data):
+    print("Error reading data from serial")
+    try:
+        errorArr = json.loads(data)
+        print(errorArr["error"])
+    except json.JSONDecodeError:
+        pass
+
+
+def process_data(data):
+    dataArr = json.loads(data)
+    temperature = float(dataArr["temperature"])
+    humidity = float(dataArr["humidity"])
+    send_sensor_data(temperature, humidity)
+
+
+def read_sensors_data_internal():
+    data = ser.readline().decode("utf-8").strip()
+    if data.startswith('{"temperature":') and "humidity" in data:
+        process_data(data)
+    else:
+        process_error(data)
+
+
 def read_sensor_data():
     try:
         while True:
-            data = ser.readline().decode("utf-8").strip()
-
-            if data.startswith("Temperature:") and "Humidity:" in data:
-                strTemperature = (
-                    data.split("Temperature:")[1].split(", Humidity:")[0].strip()
-                )
-                temperature = float(strTemperature)
-                humidity = float(data.split("Humidity:")[1].strip())
-                send_sensor_data(temperature, humidity)
-
+            read_sensors_data_internal()
     except KeyboardInterrupt:
         pass
     finally:
